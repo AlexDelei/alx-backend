673584d45c802203c2ba2dbdafdc6017d9ae3b16
AlexDelei <alexdelei125@gmail.com>
2024-06-27 00:00:24 +0300

    Least Recently used caching algorithm

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+discard the least recently used item (LRU algorithm)
+"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    discard the least recently used item (LRU algorithm)
+    """
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        LRU - discards the recently used item
+        """
+        if key is None or item is None:
+            return
+
+        # Replacing old entry with a new position later
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        self.cache_data[key] = item
+
+        # Poping the first item inserted
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last = self.cache_data.popitem(last=False)
+            print("DISACRD: ", last[0])
+
+    def get(self, key):
+        """
+        retreives value aligning with the key provided
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        # Moving accessed item to the end to mark it as
+        # recently used
+        self.cache_data.move_to_end(key)
+        val = self.cache_data[key]
+        return val
