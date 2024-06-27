0260a6d1003c7331a2248ff25f906dd7976c498a
AlexDelei <alexdelei125@gmail.com>
2024-06-27 11:48:25 +0300

    Most Recently used cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+MRU
+"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Discarding the most recently used item (MRU algorithm)
+    """
+    def __init__(self):
+        """
+        Initialize first
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache. If the cache exceeds the limit,
+        discard the most recently used item.
+        """
+        if key is None or item is None:
+            return
+
+        # Update the key-value item
+        if key in self.cache_data:
+            del self.cache_data[key]
+        self.cache_data[key] = item
+
+        # Removing the last item inserted
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            most_recent = self.cache_data.popitem(last=True)
+            print("DISCARD: ", most_recent[0])
+
+    def get(self, key):
+        """
+        Retreive an item from the cache
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
