74a81408adff3332b7df44a05b4ca2113134bbdf
AlexDelei <alexdelei125@gmail.com>
2024-07-29 09:33:06 +0300

    implemented a different algorithm to perform a LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,30 +12,32 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """initialize"""
         super().__init__()
+        self.keys = []
 
     def put(self, key, item):
         """
-        LIFO - if the limit is exceeded, then delete the last item
+        Add items to our dictionary
+        also performs the Last In First out algorithm
         """
-        if key is None or item is None:
-            return
-
-        if key in self.cache_data:
-            del self.cache_data[key]
-
-        self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            list_of_last_items = list(self.cache_data.keys())
-            last_in_item = list_of_last_items[-2]
-            self.cache_data.pop(last_in_item)
-            print('DISCARD: ', last_in_item)
+        if key and item:
+            if key in self.cache_data:
+                self.keys.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                self.evict()
+            self.cache_data[key] = item
+            self.keys.append(key)
 
     def get(self, key):
         """
-        Retreive data from the dict using the key
+        Retreive an item from the dict
         """
-        if key is None or key not in self.cache_data.keys():
-            return None
-        val = self.cache_data[key]
-        return val
+        return self.cache_data.get(key, None)
+
+    def evict(self):
+        """
+        Pops the last key from the list and removes it
+        from the cache
+        """
+        last = self.keys.pop()
+        del self.cache_data[last]
+        print('DISCARD: ', last)
