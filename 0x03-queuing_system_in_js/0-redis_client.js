6f232f6f08bee3e26d643560d714d3002d885780
AlexDelei <alexdelei125@gmail.com>
2024-07-30 18:06:27 +0300

    node redis client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,6 +3,6 @@ import { createClient } from "redis";
 const client =  createClient();
 client.on('error', error => console.log(`Redis client not connected to the server: ${error}`));
 
-if (client.connected()) {
+if (client.connect()) {
   console.log('Redis client connected to the server');
 };
