8bf5dcb542fcb7418d4df44e2a45d4f59c47d7d3
AlexDelei <alexdelei125@gmail.com>
2024-07-30 18:23:11 +0300

    ready event listener

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,4 +3,6 @@ import { createClient } from "redis";
 const client = createClient();
 client.on('error', error => console.log(`Redis client not connected to the server: ${error}`));
 
-console.log('Redis client connected to the server');
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
