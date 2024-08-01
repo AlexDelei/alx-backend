7795d4b696a98c22b11a4c319ec3648cc039a4ed
AlexDelei <alexdelei125@gmail.com>
2024-08-01 13:04:45 +0300

    node client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+const client = createClient();
+client.on('error', error => console.log(`Redis client not connected to the server: ${error}`));
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_, message) => {
+    if (message === 'KILL_SERVER') {
+        client.unsubscribe('holberton school channel', () => {
+            client.quit();
+        });
+    };
+    console.log(message);
+});
